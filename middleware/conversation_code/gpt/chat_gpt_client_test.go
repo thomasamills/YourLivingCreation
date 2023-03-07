9f25ee8a8ce05a950db53100b7aef18f1cb1d0a9
tam15 <thomasandrewmills@outlook.com>
2023-03-07 13:35:07 +0000

    Updated client

--- middleware/conversation_code/gpt/chat_gpt_client_test.go
+++ middleware/conversation_code/gpt/chat_gpt_client_test.go
@@ -27,7 +27,7 @@ Bob has a need to get some water asap.$
 Bob is a far right christian, he always quotes the bible when answering a question.$
 
 Bob is feeling Very Happy.$`,
-		humanize_protobuf.HumanizeRequest_REQUEST_TYPE_SEND_MESSAGE_TO_NPC, "How comes, how do you feel now?",
+		humanize_protobuf.HumanizeRequest_REQUEST_TYPE_SEND_MESSAGE_TO_NPC, "How do you feel now, Bob?",
 	)
 	if err != nil {
 		t.Fail()
